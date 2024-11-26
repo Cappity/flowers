6248d567a030f26274c6e8d12403ddd0bc63f5e4
Cappity <cap03586@gmail.com>
2024-11-26 21:36:40 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,14 +2,14 @@ const title = document.querySelector('.title')
 const text = 'I Have Something for you'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
-    title.innerHTML += `<span>${text[index]}<span/>`
+    title.innerHTML += `<span>${text[index]}</span>`
   } else {
-    title.innerHTML += `<span style='margin-right: 20px;'><span/>`
+    title.innerHTML += `<span style='margin-right: 20px;'></span>`
   }
 }
 
 const textElements = document.querySelectorAll('.title span');
 textElements.forEach((element) => {
-  const randomDelay = Math.random() * 3; // Menghasilkan delay acak antara 0 hingga 3 detik
+  const randomDelay = Math.random() * 3;
   element.style.animationDelay = `${randomDelay}s`;
 });
